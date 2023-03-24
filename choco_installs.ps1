f7e0df25624102b99b08d0e3354d7a135d325c39
Smithright <ryan@smithright.com>
2023-03-23 22:09:21 -0400

    add auto-update config for choco, additional choco install scripts

--- choco_installs.ps1
+++ choco_installs.ps1
@@ -1,2 +1,15 @@
-#install Java
+#install chrome
+choco install googlechrome
+
+#install java
 choco install temurin11jre -y
+
+#install .net
+
+
+
+#check list of installed packages
+choco list --local-only
+
+
+
--- install_chocolatey_package_manager.ps1
+++ install_chocolatey_package_manager.ps1
@@ -1,1 +1,9 @@
+#install chocolatey package manager
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+#set to auto-update all packages
+choco feature enable -n=autoUninstaller
+choco feature enable -n=allowGlobalConfirmation
+choco feature enable -n=autoUpgrade
+choco feature enable -n=useRememberedArgumentsForUpgrades
+
